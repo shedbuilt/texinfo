ad6039895f86929f94745a0b1dff18fdf139f78c
Auston Stewart <auston@shedbuilt.net>
2018-08-16 10:33:13 -1000

    Patching to prevent hangs with Perl 5.28.0

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Patch
+# Fix synchronization issues with Perl 5.28.0
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/0001-Update-locale-handling-for-Perl-5.28.patch" || exit 1
 # Configure
 SHED_PKG_LOCAL_PREFIX='/usr'
 SHED_PKG_LOCAL_STATIC_OPTION='--disable-static'
